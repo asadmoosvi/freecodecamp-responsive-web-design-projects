c10580cb3677d9bfbd0b8b29a782d8b3a5e3db5a
Asad Moosvi <moosvi.asad@gmail.com>
2021-07-21 21:32:11 +0500

    finish first complete version of technical documentation page

---
+++ technical-documentation-page/js/index.js
@@ -0,0 +1,24 @@
+const menuBtn = document.querySelector('.main-header__menu');
+const navBar = document.querySelector('.nav');
+menuBtn.onclick = function() {
+  navBar.classList.toggle('nav-opened');
+}
+
+// setup highlightjs
+hljs.highlightAll();
+
+const sections = document.querySelectorAll('.main-section');
+const links = document.querySelectorAll('.nav__link');
+
+window.onscroll = function() {
+  sections.forEach(section => {
+    if (pageYOffset >= section.offsetTop - 100) {
+      links.forEach(link => {
+        link.classList.remove('link-active');
+        if (link.getAttribute('href').includes(section.getAttribute('id'))) {
+          link.classList.add('link-active');
+        }
+      });
+    }
+  });
+}
